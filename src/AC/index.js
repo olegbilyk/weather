262064a80e097b278175f12e3a4e8784cc811db6
olegbilyk <oleg.bilik1994@gmail.com>
2018-02-12 00:16:18 +0200

    fix http => https

--- src/AC/index.js
+++ src/AC/index.js
@@ -3,7 +3,7 @@ import { LOAD_CITY_NOW, SEARCH_TEXT } from '../constants'
 export function loadCityNow (city) {
   return {
     type: LOAD_CITY_NOW,
-    callAPI: `http://api.openweathermap.org/data/2.5/forecast?q=${city}&cnt=20&units=metric&APPID=db6b06579dad6bdce918071f61275e19`
+    callAPI: `https://api.openweathermap.org/data/2.5/forecast?q=${city}&cnt=20&units=metric&APPID=db6b06579dad6bdce918071f61275e19`
   }
 }
 
